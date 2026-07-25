fe9c84f8788bb7a27dc76595022dd113e8e5c622
gamemaker866-create <gamemaker866@gmail.com>
2026-07-25 19:37:05 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ CATALOGO_GANAR = [
     {"id": 104, "emoji": "🍽️", "nombre": "Poner / Quitar la mesa", "recompensa": 10, "limite_diario": 2},
     {"id": 105, "emoji": "🗑️", "nombre": "Sacar la basura", "recompensa": 10, "limite_diario": 1},
     {"id": 106, "emoji": "🍽️", "nombre": "Recoger el lavavajillas", "recompensa": 3, "limite_diario": 1},
+    {"id": 107, "emoji": "👕", "nombre": "Poner el tendedero", "recompensa": 15, "limite_diario": 1},
     {"id": 108, "emoji": "🪴", "nombre": "Regar las plantas", "recompensa": 10, "limite_diario": 1}
 ]
 
