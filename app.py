776211ccb74f2ad63c1ddb763ceceb422e07c3cd
gamemaker866-create <gamemaker866@gmail.com>
2026-07-26 18:50:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,11 +67,11 @@ CATALOGO_GANAR = [
     {"id": 110, "emoji": "🫧", "nombre": "Poner la lavadora", "recompensa": 10, "limite_diario":2},
     {"id": 111, "emoji": "🛍️", "nombre": "Ir a comprar (en el barrio)", "recompensa": 5, "limite_diario":1},
     {"id": 112, "emoji": "🏃‍➡️", "nombre": "Hacer deporte", "recompensa": 10, "limite_diario": 2},
-    {"id": 113, "emoji": "📖", "nombre": "Lectura de libro (30 min)", "puntos": 10, "limite_diario": 3},
-    {"id": 114, "emoji": "📚", "nombre": "Despolvar estantería o librería", "puntos": 20, "limite_diario": 1},
-    {"id": 115, "emoji": "🔋", "nombre": "Gestionar reciclaje de pilas/electrónicos", "puntos": 15, "limite_diario": 1},
-    {"id": 116, "emoji": "♻️", "nombre": "Manualidad con envase reciclado", "puntos": 20, "limite_diario": 1},
-    {"id": 117, "emoji": "📖", "nombre": "Logro: 14 días seguidos de lectura (30 min)", "puntos": 40, "limite_diario": 1, "destacado": True}
+    {"id": 113, "emoji": "📖", "nombre": "Lectura de libro (30 min)", "recompensa": 10, "limite_diario": 3},
+    {"id": 114, "emoji": "📚", "nombre": "Despolvar estantería o librería", "recompensa": 20, "limite_diario": 1},
+    {"id": 115, "emoji": "🔋", "nombre": "Gestionar reciclaje de pilas/electrónicos", "recompensa": 15, "limite_diario": 1},
+    {"id": 116, "emoji": "♻️", "nombre": "Manualidad con envase reciclado", "recompensa": 20, "limite_diario": 1},
+    {"id": 117, "emoji": "📖", "nombre": "Logro: 14 días seguidos de lectura (30 min)", "recompensa": 40, "limite_diario": 1, "destacado": True}
     
 ]
 
